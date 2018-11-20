5a6eb1678894064902029933205cd7bce111f96f
maneeshsagar <maneeshsagar97@gmail.com>
2018-11-21 02:08:00 +0530

    "Added Keystore and Path after 2nd review"

--- app/src/main/java/com/example/maneeshsagar/apnanews/AboutActivity.java
+++ app/src/main/java/com/example/maneeshsagar/apnanews/AboutActivity.java
@@ -1,13 +1,33 @@
 package com.example.maneeshsagar.apnanews;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
-
+    public static final String MAIL="mailto: maneeshsagar97@gmail.com";
+    public static final String FEEDBACK="Send feedback";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               sendEmail();
+            }
+        });
+
+    }
+    private void sendEmail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse(MAIL));
+        startActivity(Intent.createChooser(emailIntent, FEEDBACK));
     }
 }
